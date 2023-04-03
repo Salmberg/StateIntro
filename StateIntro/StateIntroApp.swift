ad8bf189ba5ac01987b821c9703e3cc868c9a082
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-04-03 11:39:39 +0200

    Initial Commit

---
+++ StateIntro/StateIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateIntroApp.swift
+//  StateIntro
+//
+//  Created by David Salmberg on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct StateIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
